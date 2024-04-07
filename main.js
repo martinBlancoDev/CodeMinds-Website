53761e231c31aebb78ccc57226c7ae53a728e2e2
“martinBlancoDev” <martin.blanco1bf@gmail.com>
2024-04-07 12:43:37 -0300

    Se finaliza la seccion de about. Se crea html de cursos.

--- main.js
+++ main.js
@@ -20,6 +20,8 @@ function abrirPregunta(id){
     }
 }
 
+//animaciones index
+
 gsap.from('.navbar', 1.5, {opacity:0, y:-200, delay: .2})
 gsap.from('.titulo h1', 1.5, {opacity:0, y:-300, delay: .2})
 gsap.from('.titulo p', 1.5, {opacity:0, x:400, delay: .2})
@@ -28,6 +30,23 @@ gsap.from('.apoyan', 1.5, {opacity:0, x:-300, delay: .7})
 gsap.from('.bienvenida img', 1.5, {opacity:0, x:-300, delay: .7})
 gsap.from('.bienvenida-text h1', 1.5, {opacity:0, x:300, delay: .7})
 gsap.from('.bienvenida-text ', 1.5, {opacity:0, y:300, delay: .7})
+
+
+
+//animaciones about
+
+gsap.from('.about-main .imagen-ach', 1.5, {opacity:0, x:-300, delay: .2})
+gsap.from('.about-main .about-text', 1.5, {opacity:0, x:300, delay: .2})
+gsap.from('.about-main .about-text .achievements', 1.5, {opacity:0, x:-700, delay: .3})
+
+gsap.from('.valores-section .valores-titulo', 1.5, {opacity:0, x:300, delay: .8})
+gsap.from('.valores-section .valores', 1.5, {opacity:0, x:-300, delay: .8})
+
+
+
+
+
+
 
 
 
