ef4ee32799a751a55a5161f1d5624932ca023448
“martinBlancoDev” <martin.blanco1bf@gmail.com>
2024-04-09 11:00:55 -0300

    se finaliza la seccion contact Us

--- main.js
+++ main.js
@@ -49,6 +49,10 @@ gsap.from('.cursos-text', 1.5, {opacity:0, x:-300, delay: .3})
 gsap.from('.grilla-cursos', 1.5, {opacity:0, x:300, delay: .5})
 
 
+gsap.from('.contact .form-div', 1.5, {opacity:0, x:300, delay: .5})
+gsap.from('.contact .contact-info', 1.5, {opacity:0, x:-300, delay: .5})
+
+
 
 
 
