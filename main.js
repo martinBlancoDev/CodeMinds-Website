0801b64d54d69abe013ed82536946e67f0419e27
“martinBlancoDev” <martin.blanco1bf@gmail.com>
2024-04-13 21:47:47 -0300

    Se finaliza con el apartado de cursos. Se terminan de generar los html personalizados. Se crea y finaliza la sección Login y Register. Se colocan links a botones para corregir y agregar navegaciones entre la pagina.

--- main.js
+++ main.js
@@ -24,43 +24,59 @@ function abrirPregunta(id){
 
 //animaciones index
 
-gsap.from('.navbar', 1.5, {opacity:0, y:-200, delay: .2})
-gsap.from('.titulo h1', 1.5, {opacity:0, y:-300, delay: .2})
-gsap.from('.titulo p', 1.5, {opacity:0, x:400, delay: .2})
-gsap.from('.explora-btn', 1.5, {opacity:0, x:-300, delay: .5})
-gsap.from('.apoyan', 1.5, {opacity:0, x:-300, delay: .7})
-gsap.from('.bienvenida img', 1.5, {opacity:0, x:-300, delay: .7})
-gsap.from('.bienvenida-text h1', 1.5, {opacity:0, x:300, delay: .7})
-gsap.from('.bienvenida-text ', 1.5, {opacity:0, y:300, delay: .7})
+gsap.from('.navbar', 1.1, {opacity:0, y:-200, delay: .2})
+gsap.from('.titulo h1', 1.1, {opacity:0, y:-300, delay: .2})
+gsap.from('.titulo p', 1.1, {opacity:0, x:400, delay: .2})
+gsap.from('.explora-btn', 1.1, {opacity:0, x:-300, delay: .5})
+gsap.from('.apoyan', 1.1, {opacity:0, x:-300, delay: .7})
+gsap.from('.bienvenida img', 1.1, {opacity:0, x:-300, delay: .7})
+gsap.from('.bienvenida-text h1', 1.1, {opacity:0, x:300, delay: .7})
+gsap.from('.bienvenida-text ', 1.1, {opacity:0, y:300, delay: .7})
 
 
 
 //animaciones about
 
-gsap.from('.about-main .imagen-ach', 1.5, {opacity:0, x:-300, delay: .2})
-gsap.from('.about-main .about-text', 1.5, {opacity:0, x:300, delay: .2})
-gsap.from('.about-main .about-text .achievements', 1.5, {opacity:0, x:-700, delay: .3})
+gsap.from('.about-main .imagen-ach', 1.1, {opacity:0, x:-300, delay: .2})
+gsap.from('.about-main .about-text', 1.1, {opacity:0, x:300, delay: .2})
+gsap.from('.about-main .about-text .achievements', 1.1, {opacity:0, x:-700, delay: .3})
 
-gsap.from('.valores-section .valores-titulo', 1.5, {opacity:0, x:300, delay: .8})
-gsap.from('.valores-section .valores', 1.5, {opacity:0, x:-300, delay: .8})
+gsap.from('.valores-section .valores-titulo', 1.1, {opacity:0, x:300, delay: .8})
+gsap.from('.valores-section .valores', 1.1, {opacity:0, x:-300, delay: .8})
 
 
 
 //animaciones courses page
-gsap.from('.cursos-text', 1.5, {opacity:0, x:-300, delay: .3})
-gsap.from('.grilla-cursos', 1.5, {opacity:0, x:300, delay: .5})
+gsap.from('.cursos-text', 1.1, {opacity:0, x:-300, delay: .3})
+gsap.from('.grilla-cursos', 1.1, {opacity:0, x:300, delay: .5})
 
 
 //animaciones contact page
-gsap.from('.contact .form-div', 1.5, {opacity:0, x:300, delay: .5})
-gsap.from('.contact .contact-info', 1.5, {opacity:0, x:-300, delay: .5})
+gsap.from('.contact .form-div', 1.1, {opacity:0, x:300, delay: .5})
+gsap.from('.contact .contact-info', 1.1, {opacity:0, x:-300, delay: .5})
 
 //animaciones course page
 
-gsap.from('.course-main .img-banner-title', 1.5, {opacity:0, x:-300, delay: .2})
-gsap.from('.course-main .course-info', 1.5, {opacity:0, x:300, delay: .3})
-gsap.from('.course-details-section .instructor', 1.5, {opacity:0, x:300, delay: .5})
-gsap.from('.course-details-section .course-details', 1.5, {opacity:0, x:-300, delay: .5})
+gsap.from('.course-main .img-banner-title', 1.1, {opacity:0, x:-300, delay: .2})
+gsap.from('.course-main .course-info', 1.1, {opacity:0, x:300, delay: .3})
+gsap.from('.course-details-section .instructor', 1.1, {opacity:0, x:300, delay: .5})
+gsap.from('.course-details-section .course-details', 1.1, {opacity:0, x:-300, delay: .5})
+
+
+
+//animaciones del login
+
+gsap.from('.login-form ', 1.3, {opacity:0, y:-300, delay: .2})
+gsap.from('.btn', 1.3, {opacity:0, x:-300, delay: .4})
+gsap.from('.login-form h1', 1.3, {opacity:0, y:-300, delay: .2})
+gsap.from('.form-login form', 1.3, {opacity:0, x:-300, delay: .6})
+gsap.from('.form-login .login-platforms', 1.5, {opacity:0, x:300, delay: .6})
+gsap.from('.form-login p', 1.3, {opacity:0, x:-300, delay: .6})
+gsap.from('.title-login p', 1.3, {opacity:0, y:300, delay: .6})
+
+
+
+
 
 
 
